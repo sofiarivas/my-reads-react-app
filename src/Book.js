28410f3c4c0e40ba352b3283fb3c11f269a62d5c
sofiarivas <sofi.rivasm@gmail.com>
2018-03-14 06:51:13 +0000

    Fix image thumbnail url on book component

--- src/Book.js
+++ src/Book.js
@@ -5,7 +5,7 @@ class Book extends Component {
   return (
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.details.image})` }}></div>
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.details.imageLinks.thumbnail})` }}></div>
         <div className="book-shelf-changer">
           <select>
             <option value="none" disabled>Move to...</option>
